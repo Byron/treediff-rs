60ef3cf804308958c4650466359c6ae337d687b0
Sebastian Thiel <byronimo@gmail.com>
2017-01-15 15:07:40 +0100

    Added Value for HashMap
    Just for completeness.
    All other std types are just a matter of time.
    It's more important now to get the entire breadth
    of the implementation ready, before filling in
    some more datatype support.

--- src/traitimpls/std.rs
+++ src/traitimpls/std.rs
@@ -1,4 +1,6 @@
 use traitdef::Value;
+use std::collections::HashMap;
+use std::hash::Hash;
 
 impl Value for String {
     type Item = Self;
@@ -15,5 +17,16 @@ impl<T> Value for Vec<T>
     type Key = usize;
     fn items<'a>(&'a self) -> Option<Box<Iterator<Item = (Self::Key, &'a Self::Item)> + 'a>> {
         Some(Box::new(self.iter().enumerate()))
+    }
+}
+
+impl<K, T> Value for HashMap<K, T>
+    where T: PartialEq,
+          K: Clone + Hash + Eq
+{
+    type Item = T;
+    type Key = K;
+    fn items<'a>(&'a self) -> Option<Box<Iterator<Item = (Self::Key, &'a Self::Item)> + 'a>> {
+        Some(Box::new(self.iter().map(|(k, v)| (k.clone(), v))))
     }
 }
--- tests/std_value.rs
+++ tests/std_value.rs
@@ -1,6 +1,7 @@
 extern crate treediff;
 mod std_value {
     use treediff::Value;
+    use std::collections::HashMap;
 
     fn assert_non_scalar<V>(l: V)
         where V: Value
@@ -35,5 +36,19 @@ mod std_value {
         assert_eq!(l.items().unwrap().collect::<Vec<_>>(),
                    vec![(0, &String::from("one"))]);
         assert_non_scalar(l);
+    }
+
+    #[test]
+    fn hashmap() {
+        let mut l = HashMap::new();
+        l.insert("hello", 123);
+        l.insert("there", 42);
+
+        let sorted = {
+            let mut s = l.items().unwrap().collect::<Vec<_>>();
+            s.sort_by_key(|v| v.0);
+            s
+        };
+        assert_eq!(sorted, vec![("hello", &123), ("there", &42)]);
     }
 }
