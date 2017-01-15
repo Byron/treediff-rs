5ac03ceaa60e4a1bf42ed1375ef16e906a7cd050
Sebastian Thiel <byronimo@gmail.com>
2017-01-15 14:10:12 +0100

    Added JsonKey to abstract over usize/String
    Arrays have usize indices, obejcts use String.
    To make them comparable, it's most efficient to
    use an enum.

--- src/traitimpls/json.rs
+++ src/traitimpls/json.rs
@@ -1,12 +1,16 @@
 use traitdef::Value;
 use rustc_serialize::json::Json;
 
+pub enum JsonKey {
+    String(String)
+}
+
 impl Value for Json {
     type Item = Json;
-    type Key = String;
+    type Key = JsonKey;
     fn items<'a>(&'a self) -> Option<Box<Iterator<Item = (Self::Key, &'a Self::Item)> + 'a>> {
         match *self {
-            Json::Object(ref inner) => Some(Box::new(inner.iter().map(|(s, v)| (s.clone(), v)))),
+            Json::Object(ref inner) => Some(Box::new(inner.iter().map(|(s, v)| (JsonKey::String(s.clone()), v)))),
             _ => unimplemented!(),
 
         }
