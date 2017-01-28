420e19c2af2e31a3724c2caef790303d5f66cbbb
Sebastian Thiel <byronimo@gmail.com>
2017-01-28 15:09:01 +0100

    Revert "Added futile attempt to workaround type issue"
    This reverts commit 01a4b58f5fed5f02ad1f55e1f12a5606d3e024b2.

--- src/diff.rs
+++ src/diff.rs
@@ -2,42 +2,19 @@ use traitdef::{Value, Delegate};
 use std::cmp::Ordering;
 use std::collections::BTreeSet;
 
-#[derive(PartialEq)]
-enum Values<'a, V1: 'a, V2: 'a> {
-    V1(&'a V1),
-    V2(&'a V2),
-}
-
-impl<'b, V1, V2> Value for Values<'b, V1, V2>
-    where V1: Value<Key = V2::Key, Item = V2::Item>,
-          V2: Value
-{
-    type Key = V1::Key;
-    type Item = V1::Item;
-    fn items<'a>(&'a self) -> Option<Box<Iterator<Item = (Self::Key, &'a Self::Item)> + 'a>> {
-        match *self {
-            Values::V1(ref v) => v.items(),
-            Values::V2(ref v) => v.items(),
-        }
-    }
-}
-
-pub fn diff<'a, V, D, T>(l: &'a V, r: &'a V, d: &'a mut D)
+pub fn diff<'a, V, D, AnyV>(l: &'a V, r: &'a V, d: &mut D)
     where V: Value,
           <V as Value>::Key: Ord,
           <V as Value>::Item: Value,
-          D: Delegate<'a, Values<'a, V, <V as Value>::Item>>
+          D: Delegate<'a, V>
 {
     match (l.items(), r.items()) {
         // two scalars, equal
-        (None, None) if l == r => {
-            let v = Values::V1(l);
-            d.unchanged(&v)
-        }
+        (None, None) if l == r => d.unchanged(l),
         // two scalars, different
-        (None, None) => d.modified(&Values::V1(l), &Values::V1(r)),
+        (None, None) => d.modified(l, r),
         // two objects, equal
-        (Some(_), Some(_)) if l == r => d.unchanged(&Values::V1(l)),
+        (Some(_), Some(_)) if l == r => d.unchanged(l),
         // two objects, different
         (Some(li), Some(ri)) => {
             let mut sl: BTreeSet<OrdByKey<_, _>> = BTreeSet::new();
@@ -48,7 +25,7 @@ pub fn diff<'a, V, D, T>(l: &'a V, r: &'a V, d: &'a mut D)
                 let v1 = sl.get(k).expect("union to work");
                 let v2 = sr.get(k).expect("union to work");
                 if v1.1 == v2.1 {
-                    //                    d.unchanged(v1.1);
+                    d.unchanged(v1.1);
                 }
             }
         }
