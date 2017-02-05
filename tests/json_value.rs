1070cd40611ac7ad0e79beab5bb94c0e8e7551ee
Sebastian Thiel <byronimo@gmail.com>
2017-02-05 07:55:15 +0100

    Added json value suite
    To allow bringing in serde without duplication.

--- tests/json_value.rs
+++ tests/json_value.rs
@@ -1,49 +1,52 @@
 extern crate treediff;
 
 macro_rules! make_suite {
-    ($module:ident) => {
-    mod $module {
+    ($make:ident, $index:tt, $null:expr, $bool:expr) => {
+        #[test]
+        fn scalar_values() {
+            for v in &["null", "true", "1.23", "-1234456", "1234456", "\"string\""] {
+                let j = $make(v);
+                assert!(j.items().is_none());
+            }
+        }
 
-    }
+        #[test]
+        fn array() {
+            let j = $make(r#"[null, true]"#);
+            assert_eq!(j.items().unwrap().collect::<Vec<_>>(),
+                       vec![($index::Index(0), &$null),
+                            ($index::Index(1), &$bool(true))]);
+        }
+
+        #[test]
+        fn object() {
+            let j = $make(r#"{"a": null, "b": true}"#);
+            assert_eq!(j.items().unwrap().collect::<Vec<_>>(),
+                       vec![($index::String("a".into()), &$null),
+                            ($index::String("b".into()), &$bool(true))]);
+        }
+
+        #[test]
+        fn empty_object() {
+            let j = $make(r#"{}"#);
+            assert_eq!(j.items().unwrap().collect::<Vec<_>>(), vec![]);
+        }
     };
 }
 
+
 #[cfg(feature = "with-rustc-serialize")]
-mod rustc_json_value {
+mod rustc_serialize {
     extern crate rustc_serialize;
-    use treediff::Value;
     use treediff::value::json::rustc_serialize::JsonKey;
     use self::rustc_serialize::json::Json;
+    use treediff::Value;
 
-    make_suite!(rustc_value);
-
-    #[test]
-    fn scalar_values() {
-        for v in &["null", "true", "1.23", "-1234456", "1234456", "\"string\""] {
-            let j: Json = v.parse().unwrap();
-            assert!(j.items().is_none());
-        }
+    fn make_json(v: &str) -> Json {
+        v.parse().unwrap()
     }
 
-    #[test]
-    fn array() {
-        let j: Json = r#"[null, true]"#.parse().unwrap();
-        assert_eq!(j.items().unwrap().collect::<Vec<_>>(),
-                   vec![(JsonKey::Index(0), &Json::Null),
-                        (JsonKey::Index(1), &Json::Boolean(true))]);
-    }
+    make_suite!(make_json, JsonKey, Json::Null, Json::Boolean);
 
-    #[test]
-    fn object() {
-        let j: Json = r#"{"a": null, "b": true}"#.parse().unwrap();
-        assert_eq!(j.items().unwrap().collect::<Vec<_>>(),
-                   vec![(JsonKey::String("a".into()), &Json::Null),
-                        (JsonKey::String("b".into()), &Json::Boolean(true))]);
-    }
 
-    #[test]
-    fn empty_object() {
-        let j: Json = r#"{}"#.parse().unwrap();
-        assert_eq!(j.items().unwrap().collect::<Vec<_>>(), vec![]);
-    }
 }
