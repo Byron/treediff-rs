17fd836d45f6f38c400994d0912b4a1a248b89f4
Sebastian Thiel <byronimo@gmail.com>
2017-02-05 17:31:28 +0100

    Added some crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
-//! See what's different in arbitrary data structures.
+//! **See what's different in arbitrary data structures**.
+//!
+//! The [main diff algorithm][diffalgo]
+//! we implement here is less than a screen full of lines, yet it enables a vast amount of
+//! applications.
+//! It can work with all values implementing the `Value` trait.
+//!
+//! On top of it there is a [merge implementation][merge], which can be used to aggregate changes
+//! into a target value. It works with `Value`s which implement the `Mergeable` trait.
+//! [merge]: https://github.com/Byron/treediff-rs/blob/master/src/merge.rs
+//! [diffalgo]: https://github.com/Byron/treediff-rs/blob/master/src/diff.rs
 #[cfg(feature = "with-rustc-serialize")]
 extern crate rustc_serialize;
 #[cfg(feature = "with-serde-json")]
