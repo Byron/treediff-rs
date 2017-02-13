f06102825d04bf245b5ff2ee931e8a45b94e9dd3
Sebastian Thiel <sebastian.thiel@thoughtworks.com>
2017-02-13 14:06:29 +0100

    doc(lib): fix links

--- changelog.md
+++ changelog.md
@@ -1,3 +1,8 @@
+<a name="v2.1.1"></a>
+### v2.1.1 (2017-02-13)
+
+* fix library documentation links
+
 <a name="v2.1.0"></a>
 ## v2.1.0 (2017-02-13)
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
 //! **See what's different in arbitrary data structures**.
 //!
-//! The [main diff algorithm][diffalgo]
+//! The main [`diff`] algorithm.
 //! we implement here is less than a screen full of lines, yet it enables a vast amount of
 //! applications.
 //! It can work with all values implementing the `Value` trait.
 //!
-//! On top of it there is a [merge implementation][merge], which can be used to aggregate changes
+//! On top of it there is a [`merge`] implementation, which can be used to aggregate changes
 //! into a target value. It works with `Value`s which implement the `Mutable` trait.
 //!
 //! # Usage
@@ -14,8 +14,8 @@
 //! Also note that you will have to choose the features to build the library with in order to
 //! get trait implementations for `Value` types of common libraries,
 //! i.e. `cargo build --features=with-serde-json`.
-//! [merge]: https://docs.rs/treediff/*/treediff/merge/index.html
-//! [diffalgo]: https://docs.rs/treediff/*/treediff/fn.diff.html
+//! [`diff`]: fn.diff.html
+//! [`merge`]: tools/struct.Merger.html
 //! [diff-tests]: https://github.com/Byron/treediff-rs/blob/master/tests/diff.rs
 //! [merge-tests]: https://github.com/Byron/treediff-rs/blob/master/tests/merge.rs
 #![deny(missing_docs)]
