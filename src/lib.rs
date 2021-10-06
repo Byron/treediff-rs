8f28c9294f91296724d88a0217b8d908a4b41008
Martin Vilcans <martin@resolutiongames.com>
2021-10-06 11:31:04 +0200

    Fix broken doc comments.
    Maybe it broke with a Rust update, as rustdoc now supports linking
    [`like_this`].

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
 //! **See what's different in arbitrary data structures**.
 //!
-//! The main [`diff`] algorithm.
+//! The main [`diff`](`diff::diff`) algorithm.
 //! we implement here is less than a screen full of lines, yet it enables a vast amount of
 //! applications.
 //! It can work with all values implementing the `Value` trait.
 //!
-//! On top of it there is a [`merge`] implementation, which can be used to aggregate changes
+//! On top of it there is a [merge][`tools::Merger`] implementation, which can be used to aggregate changes
 //! into a target value. It works with `Value`s which implement the `Mutable` trait.
 //!
 //! # Usage
-//! Please have a look at the tests for [diff][diff-tests] and [merge][merge-tests] tests.
+//! Please have a look at the tests for [diff][diff-tests] and [merge][merge-tests].
 //!
 //! Also note that you will have to choose the features to build the library with in order to
 //! get trait implementations for `Value` types of common libraries,
 //! i.e. `cargo build --features=with-serde-json`.
-//! [`diff`]: fn.diff.html
-//! [`merge`]: tools/struct.Merger.html
+//!
 //! [diff-tests]: https://github.com/Byron/treediff-rs/blob/master/tests/diff.rs
 //! [merge-tests]: https://github.com/Byron/treediff-rs/blob/master/tests/merge.rs
 #![deny(missing_docs, unsafe_code)]
